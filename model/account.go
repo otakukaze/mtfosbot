51c3ed0e661c5667661459665d6f1b9e70c9a39b
Jay <admin@trj.tw>
2018-12-02 11:46:16 +0800

    add change password and create accoutn model

--- model/account.go
+++ model/account.go
@@ -2,6 +2,7 @@ package model
 
 import (
 	"database/sql"
+	"errors"
 	"time"
 )
 
@@ -33,5 +34,28 @@ func GetAccount(account string) (acc *Account, err error) {
 	if err != nil {
 		return nil, err
 	}
+	return
+}
+
+// CreateAccount -
+func CreateAccount(account, password string) (acc *Account, err error) {
+	acc = &Account{}
+	err = x.Get(acc, `insert into "public"."account" ("account", "password", "ctime", "mtime") values ($1, $2, now(), now())`, account, password)
+	if err == sql.ErrNoRows {
+		return nil, nil
+	}
+	if err != nil {
+		return nil, err
+	}
+	return
+}
+
+// ChangePassword -
+func (p *Account) ChangePassword(password string) (err error) {
+	if len(password) == 0 {
+		return errors.New("password is empty")
+	}
+	_, err = x.Exec(`update "public"."account" set "password" = $1, "mtime" = now() where "id" = $2`, password, p.ID)
+	p.Password = password
 	return
 }
